5bbe5cdc9594a5d5a0b1f57f6910721f8f054f50
Jeff Scott Brown <brownj@objectcomputing.com>
2018-06-05 11:38:30 -0500

    M1 compatibility

--- grails/ui/grails-app/init/ui/BootStrap.groovy
+++ grails/ui/grails-app/init/ui/BootStrap.groovy
@@ -1,8 +1,12 @@
 package ui
 
+import io.micronaut.core.convert.ConversionService
+import io.micronaut.reactive.rxjava2.converters.RxJavaConverterRegistrar
+
 class BootStrap {
 
     def init = { servletContext ->
+        new RxJavaConverterRegistrar().register(ConversionService.SHARED)
     }
     def destroy = {
     }
--- grails/ui/src/main/groovy/httpdemo/SynthClient.java
+++ grails/ui/src/main/groovy/httpdemo/SynthClient.java
@@ -1,11 +1,12 @@
 package httpdemo;
 
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
 import io.micronaut.http.client.Client;
 
 import java.util.List;
 
-@Client(id="synthData")
+@Client("synth-data")
 public interface SynthClient {
 
     @Get("/synths")
